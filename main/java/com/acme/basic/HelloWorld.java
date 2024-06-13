530bfbd12b4da051e01ee0e5a0b480fa1a2ffbc4
CursoInap <cubillobp@gmail.com>
2024-06-13 13:50:05 +0200

    Update HelloWorld.java PRUEBA

--- main/java/com/acme/basic/HelloWorld.java
+++ main/java/com/acme/basic/HelloWorld.java
@@ -4,10 +4,12 @@ public class HelloWorld {
 
   void sayHello() {
     System.out.println("Hello World!");
+    System.out.println("Hello World!"); //DUPLICO
   }
 
   void notCovered() {
     System.out.println("This method is not covered by unit tests");
+    System.out.println("Hello World!"); //DUPLICO
   }
 
 }
